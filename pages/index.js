7ba05a3da0d495d875459494ffadc89ec637a00a
Edgar Duran-Perez <eduranperez@ucdavis.edu>
2022-01-10 09:06:57 -0800

    initial working webosocket connection

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,29 @@
-import Head from 'next/head'
+/*import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+var W3CWebSocket = require('websocket').w3cwebsocket;
+
 
 export default function Home() {
+
+  var client = new W3CWebSocket('wss://70v6q0ruz1.execute-api.us-west-2.amazonaws.com/Prod');
+  client.onopen = function() {
+    console.log('WebSocket Client Connected');
+
+    var test = { action: "sendmessage", data: "hello world" };
+    client.send(JSON.stringify(test));
+};
+
+client.onclose = function() {
+    console.log('echo-protocol Client Closed');
+};
+
+client.onmessage = function(e) {
+    if (typeof e.data === 'string') {
+        console.log("Received: '" + e.data + "'");
+    }
+};
+
   return (
     <div className={styles.container}>
       <Head>
@@ -66,4 +87,68 @@ export default function Home() {
       </footer>
     </div>
   )
-}
+}*/
+import React, { useState, useCallback, useEffect } from 'react';
+import useWebSocket, { ReadyState } from 'react-use-websocket';
+
+export default function WebSocketDemo(){
+  //Public API that will echo messages sent to it back to the client
+  const [socketUrl, setSocketUrl] = useState('wss://70v6q0ruz1.execute-api.us-west-2.amazonaws.com/Prod');
+  const [messageHistory, setMessageHistory] = useState([]);
+
+  const {
+    sendMessage,
+    lastMessage,
+    readyState,
+  } = useWebSocket(socketUrl, {
+    onOpen: () => {
+      console.log('opened');
+      console.log("Sending user id");
+      sendMessage(JSON.stringify({ action: "sendmessage", data: {event: "open_connect", userId: "eduranperez@ucdavis.edu"} }))
+    },
+    //Will attempt to reconnect on all close events, such as server shutting down
+    shouldReconnect: (closeEvent) => true,
+  });
+
+  useEffect(() => {
+    if (lastMessage !== null) {
+      setMessageHistory(prev => prev.concat(lastMessage));
+    }
+  }, [lastMessage, setMessageHistory]);
+
+  const handleClickChangeSocketUrl = useCallback(() =>
+    setSocketUrl('wss://70v6q0ruz1.execute-api.us-west-2.amazonaws.com/Prod'), []);
+
+  const handleClickSendMessage = useCallback(() =>
+    sendMessage(JSON.stringify({ action: "sendmessage", data: "hello world" })), []);
+
+  const connectionStatus = {
+    [ReadyState.CONNECTING]: 'Connecting',
+    [ReadyState.OPEN]: 'Open',
+    [ReadyState.CLOSING]: 'Closing',
+    [ReadyState.CLOSED]: 'Closed',
+    [ReadyState.UNINSTANTIATED]: 'Uninstantiated',
+  }[readyState];
+
+  return (
+    <div>
+      <button
+        onClick={handleClickChangeSocketUrl}
+      >
+        Click Me to change Socket Url
+      </button>
+      <button
+        onClick={handleClickSendMessage}
+        disabled={readyState !== ReadyState.OPEN}
+      >
+        Click Me to send 'Hello'
+      </button>
+      <span>The WebSocket is currently {connectionStatus}</span>
+      {lastMessage ? <span>Last message: {lastMessage.data}</span> : null}
+      <ul>
+        {messageHistory
+          .map((message, idx) => <span key={idx}>{message ? message.data : null}</span>)}
+      </ul>
+    </div>
+  );
+};
